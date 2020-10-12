d2c60d28364203cc0b286459b9b53f8c29ba2a82
minh2804 <minh2804@uw.edu>
2020-10-11 19:19:39 -0700

    Add examples data

--- Kanalysis/examples/titanic.R
+++ Kanalysis/examples/titanic.R
@@ -1,10 +1,14 @@
+# Import the data
 data <- read.csv("../external/titanic/inst/data-raw/train.csv")
 
-dependent_var <- "Survived"
-independent_vars <- c("Pclass", "Sex", "Age", "SibSp", "Parch", "Fare")
-data <- data[, c(dependent_var, independent_vars)]
+# Filter out the variables
+y <- "Survived" # Dependent variable
+x <- c("Pclass", "Sex", "Age", "SibSp", "Parch", "Fare") # Independent variables
+data <- data[, c(y, x)]
 
+# Clean the data
 data <- na.omit(data)
 data[, "Sex"] <- as.numeric(as.factor(data[, "Sex"])) # 1 = female, 2 = male
 
+# Export the data
 write.csv(data, "data/titanic.csv", row.names = FALSE)
